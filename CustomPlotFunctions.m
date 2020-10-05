6d8fc17879af5c113e62d3e223638e8aa0611aa5
Supratim Das Bakshi <sdbakshi13@gmail.com>
2020-10-05 13:30:10 +0530

    updated

--- CustomPlotFunctions.m
+++ CustomPlotFunctions.m
@@ -1,106 +1,126 @@
-(*!1N!*)mcm
-j<hTJue'P+lKh]7t>X^Ce>E2FtTGjEmi2X^Ue>E2FtTGjEmi2X^Ue>E2FtTGjEmi2X^Ue>
-E2FtTGjEmi2X^Ue>E2FtTGjEmi2X^Ue>E2Ft)<Yb2V4G6JDZMVk~\$0_?@`l'^"C0[&l[n
-R}' /(aAHEK7A1-YlRGY.$S?^|_.\mFPt'dj:.[\Pt2kLahzsh(?DN9shT/ Bm/To}!-@j
-ERi8$vBQ;3#4ivDzOj<pTFAf'C[nR}$=G>"?.3#tEu;?/SNS7O!sLti5$vt{7/@orJ8bZy
-Dh]X8S,W(_sK!e.f,5lL\G$}_;pxj'Dz]XivDz]XivDz]XivDzOj<pTFivDz]XivDz]Xiv
-Dz]XivDz]XivDz]Xiv+yR?B^M_cM/AF#Af-rg's^L&'$G_qGIn,Yhm5ZuM$2;+$0BGEA?B
-Gi9\fp_6\mFP!t2zfUp,0(U#IvWLBqNhACO)".@pE;MZ( JCITQL*(?P(I/E7Oi";|]U8S
-,W>5)y;g`2K61AB&"N")J&9,N6NkO.bC: oUQ+b+HLZ@0X_eQG 6Rs\Q@iQ(9/1?I5`=iv
-Dz]XivDz]XivDz]XivDz]XivDzH#CFm"ioDz]XivDz]XivDz]XivDz]XivDz]Xivj`>F^C
-ibkFU*RQ$C_tm<szmia,dVD{n::8;9(ILrMO]HauPel%SJ/_kQitDz]X_,\mFP7Ja9O9bC
-o[B<O5V<&k^/"b/v@z=giD<<ia?zWmZ]O?.2[,WL5ZD|E@Z>j<`L!$&j6Y(YZ1:-abh\TQ
-X4BNNP@)>"'Mb@j"!y!|Lti5$vah?D.%iy)?Yb2V<oTv_ %Wn^E#]XivDz]XivDz]XivDz
-]XivDzE@Z>j<mi2X^Ue>E2FtTGjEmi2X^Ue>E2FtTGjEmi2X^Ue>E2FtTGjEmi2X^Ue>E2
-FtTGjEmi2X^Ue>E2FtTGjEmi2X^UOh]1=3E.oE`5*3F"h%Y0QuZr`X;3g3h]7tC}X2 @=Q
-PkCcH")`qc(=/Xh/8#/QuljE5Jp= /Yo`Zhh@V0|ufuYQ5o*n^t9tcMO6\&7ir^e7rJqg)
-4*hf1\6A1WNe#&-K?u&-i!t]MO6\&7Fo<Cpr@8fU#/f?NDa%bAoI h!$UCj&s!q\-KO-@)
-Dh9\"Au]W;.jQ74r;gggIHbC%k&P#)Vl9@\^Bx$N]O;?oN&k72B*.$aY5CDa3shf-XND#S
->B ;he/`_ 0&a%-bMMT6i!e48>QLDBkIp$jV50g~bc'j`3]ul{oE5#\u<E^~JAE%dsaG }
-c;/BKTp,Z=Fod,*!HQn)n-3{ ]W~#P[Q*}g+k,CaT;c3:.'H&\NNRApd50]]BIkn NV"]<
-9/\J[&:8kio?`5,Ub6bPbaA1ZmpGfl&P#)Vl9@\^XN9{,3)`fUWLcZDi8B[s ~h!IHbCPv
-bAK;N<[mgrkE0&*"ugNmY:DhW^Vt8c%trxq\-K9{,3k_76-d!#t.:;11-vC8+E!b'fR~-Y
-!~o55D3v'6Ewg#c::.8Unf')W~pG5e OrY6K<>6w)4u5f`@es*,1'?kPA1nA0[(D1#LB!$
-m_nXf(%>M[r6o>%Jc#IHbC%kKUD,Ux3r,fhTM+^"!c;7&RADuA/dYv2lny_nA1u(hy7X'q
-`3X <f]bH3v4QN426tYlQ)_O8we`JXWNP/fEND+/Jy]O Sbam,(=u#'|DN5G3u8PA>("=v
-YxT=Q+gg:.?}g #Z2{hf-XcyN-@]rV9E*tc/IHbCsy[^O2WKj&!R7]g$Z1h'>5JtM*T6PL
-$|fl1 &S`u9g6+L!*EOb_/ppr[R_V0\n+4uVtG/zh|JnGkQ!QoT=!iAW`LS/a7EY6v[8;2
-Q]&%a<t<X!Vchj0aYN2LrJK8_g=E?q9]<6)H8A($Js8d^^o1]1rqA{bg&Fo1?6Y~V!R>\1
-=[n0gl>5JtM*T6PL$|;a2E=b:R-M<z`2YE'1s/D\j.K0$3(Qa< Q:#'"K/D"&)u='|DN5G
-3u8PiVh6)zoN&km(b10>QCEW&-(@qL?v[cu^oW+kCXK~oTK/@"nSbm@:7R<ABau>'|DN5G
-3u8Pd#:. t0kYNEoA\%{'u4t]o!CQHeCUp+SGA6>uVG0+\U{ C;N/yRL'{AB*<oN&km(b1
-0>J\CUXkGgZ&h'h\ZCi;@T_ GFKaXVG$`18Bt,G& RP(-n3v0hnNUt C/ESKVbNMNMt?fx
-O6@AM4OCkhKCv*O{hh@V0|ufuY/S9V1z"()yD][E)yj#W~n{aIdu!ghs%Nn`EUsr/zh|Jn
-GkQ!\va*GiLN3_3w1&'"@l Gsg8b`?6s__JXiC`@rJ`%,)?VAXd,$|$2S{Wb5`5<*"f8ZY
-5l)IoN&k72B*.$aY5CDaAAZ&<{cC0Jnc4)mF79*~g+k,CalKBR;CnPGTm^\I?2;Id(SJ:1
-"fG@]uiX3*(\Q{I1?JH|c>+B.d:`i7Pd;FnZHz"]t %g`i^E<(V&5Mj&>EGjFp7Phy;AoP
-s1+<AF2y*p+ g+CDJeYg1\tc2jGkmR/X'".`.x1.tc.n+j#%-ED[%OsgIp83MpS0uQ^g!i
-0k==D"QD7'P`MVtcJ>6gVIN6"6A=X>E"`hi [,sk#Ht~`#gDfLO,5<Da@0t@k-FD@)d:B,
-0Mc}hU`16X@tZ-@:VE-%t6$.M>0}K B2Vlf}M2T6*N`= 8j(kRpLu$(.Jr.k6|257@$w^l
-A}3^mI55/P\v,5bZ&x/Wu\#}27u&M1G'Qz?Ph=]\a0Pm0.`:U%nEbD=PMyVt&qq %(Y1oq
-hsO(;I.BGA&VsXfMNSKC!Oh4tS$.?pO#<^E.H"nxSJ7U2LrJK8:viM[R'zVsbjJbjsm,;a
-X)TWa)NM;I"&r&TWBB<Pnb--pza.cqfv@Kuk1Oukt~<4Yh2%TCUsTYT ,V\n,5\]Bo%|W*
-t=q\-KRk8F1z,rubu#^"g/Pv'g;fccpOGc DY1>0?pMVMAOCkhKCv*uKlHO6KHQP5LC4a`
-?mO#i+'P0~UB5?.&N"^Fa"-b"BHln)2Q,1.2+E+{':RO0^h|JnM*T6jrt2oRgb,AVJ oWq
-Zr0mQL6@:+kOJApT+kl\2y&kn?_nA1cV50LK8Ei"Op-N7_aU.dAG/fps50'@&! \s:Rzfp
-`WL]JN!R]yFUGl"fgc76i avbPbaA1j}Q/LC8).T.hA>[cqZpA$/tc@8VE-%,+LB&wS_nC
-G<VG-%810'K1=vo4bQ4C&e&eQpA8r:?A=qC|R?B^?vTQa=hIJ59,N6NkO.bC?EjYWD\1(6
-m*oE$LrM8d^^o1]1Y`hf-XND(xndQJG'h-D-X'+%9FrT?v[cLUBLK~oTgE";lb_nA1"u5j
-]1f')x&CD:6U?! q-[NDuFD^\lnSmdqPk^9{72?!8dlmUb%BSOU+I^n)3iH`A=("s2j;jX
--F2E#Ts?%;p(i:^& +Hg,bPUJAD>)cqc8%jlt23R:T`oCTK~oT"fG@]uiX-&0l<)L|!^l<
-,Wtq%;p(i:^& +5dYxT5/-5^A8\yXNbQC3mec(G2l=Di8B%}@-/gg"/ph/*utK/zh|qU(;
-f(;@Gm>Sj)" 6m`ZnEfplw'C$glj_\L.T5["&(?-sM\t)r2{VsMOI)SP3qX8'EgJA81YQ]
-d6hUHe/`AkuA[0&(#W>B ;he/`_ 0&a%-bMMT6@(k|Ph[m)LPS=C'peUEt,3J~/z 0u\#}
-27;,6w)4u5f`@es*,1'?kPA1(;AD[8n\gr>5JtM*T6PL$|;aSJ7U<b.C?Vr)porDqLSvbM
-L]uQiRGu_=u9u#]M,5HiQpU+sTuZZ~E}[7;2Q]&%a<nvg)cZtL:%gb!RX-q^dB`28BqI?v
-[cu^oW+kBw;f@`u\#}27*c)zWCm1P+04nt`2uA`5;toN&km(b10>J\CUXkGgYEY:j,SQLY
-!|:jEZg]R2:.#$Us@:VE-%,+LBrC9l72[@ADmL\sWx!0nr_nA1u(hy7X:tDj')P(Y*gqa7
-EY6v7T-d8jn}+zN/+jPM#(=EU!s)?vZ-(@6BuAYkGWQr\1h2ih.3tH9?[8;2Q]&%a<t<X!
-VchjY*<fiy\?Ev 7d"lA6>usVZ$!B=3^W[(BWZVt&qi"`Q6T:+86"NnQSZND@Jbi@:U~<S
-Bau>'|DN5G3u8PiV@T3tUv=Ut=Y+<fiy\?Ev 70nB&"N")1u<) 0Ys8ErR?v[cu^oW+kCX
->9Gn[7;2Q]&%a<9!`*8UAAc/IHbCsy[^O2X,#P[Q 3IsWL(WJunc<1B-.$aY5CDau5t]Vr
-'6Ewv4A}[;6YblEs5av07~DD0;SYJrJlRtWu%9!$P(-n3v0hnNSr C/ESKVbNMNMt?;mO<
-@A0w'"@lqXPFT=Q++{:-'H&\Ael&50hf-XcyN-@]1u<)L|!^STcQYG2>=b:R-M<z`2tc5e
-U$GFa70_O,tcE-pGPh[m)LPS=C'peUEt,3J~/z 0u\#}27;,D][EjZ3b`{(@Ju 52$CooI
-FT7P-^cKauJA`:VN;47+>xVsj#-9?6N"0"EK"u0rq.Ub0&C1&lPu=;C:s1EVoWdn?2.$=A
-=qB<s1=^7U&*:ji7*~q9sbe{)lY4mF79T@!iI_c>R")z(D<LP`MVDx^]/d_Yli%`'B-@D[
-cM-bG=_X!>J M~kO?2.$;#*fp.$Grvq\-KVsoQKCHs:/6?5wX `#gDb`e&Vf;_EhSds&+A
-@ c/A8($-R%Cs2T=s;U:GFa70_O,tc/[da>(ZGj<hTJ54Gl$Ys<+-V?PJ{VGX0[&Ojg{e5
-GF+ATkshs.oT7?$w/XV]oLL>!`C:-'k]9;*t)`25/XW~H5+D/l/VIf_ _ieZ_YtA'~VsV^
-cAR$A1Xkt\WY(Bu`M[3q@Rme_P+ RJ0$iv(AJ/*8rxq\-KDJ 2gn:*RaUy[f>LgBA81YQ]
-1[PmN*[1k~a(SxND^"QI"dqL?A8L7%hcCT>9]t9hpKR"4#q#%;H1d":. t_zA^-%u^&495
-]w#&DJ^<rJ`%li`2U<8elmUbT2c):.'H&\Ael&50Boq$\ta*Gi5W[,rDj'kRc9+w/kIiLJ
->,-8,rC622I_"]v"c$,Z!%0n==D"QDl|*WM&Y|(@dnUh2@c?WMqQPFe6?0%'"/baA1s2az
->(ZGj<hTJ54Gl$Ys<+-V?PJ{VGX0[&Ojg{e5GF+ATkshs.oTD(.1GAFN%~@-)`-I7O2E!R
-=t rmF[SFO0i_,$oH1F?k4hso$GTmcg4_~'o&+5JWM&j72iN-dbwqi4+h\!R25l{!nZ[J.
-irbpnVr%[kQP5L@q!TY1DJ\V DNkg<UO'EVYT5VjMV7{J>$A#BfT:.a%RW5iP(nE79+}`:
-9ge`_yA^H"W>j&L]JA^f:mDj')pH*W25qZQh==`28U@pc6W|q@*WrIqN9E*ttch\3GDj[E
-U%++$ws2`YO,Sv78W@Djg>Q/aAi t]MO6\&7:#P+2sDj[EU%:zYuJ.s23|Lg5<RzG\ACN@
-@F_"JArrF%=I0r*JEh[ .$8As1?0a9O9bCt0BkJM9E*ttci!c_q6BR@0-YE[4SR>h=cmGt
-h%cz)L_m2lLahzsh(?DNrljd>Fl'#ZPKDp:"pKGi.CAaS@%?/NndT{V#iVo%Dkr]0)Wvir
-e/Dx5*u|:Ir%2rC"T""Uc{'ll150'@&!^Z#h_f?AKq9iIpXSO,Sv78W@Dj,#Xg_WgDQ/aA
-i :s)0"fG@]uiX-&3wb N}tABkC&_2?E=8XUYf^TXs0+Wv?XZmYfg-<rjEgTu23ReRkNUq
-C\*8--?6N"tc=A8$m}/%rNG1"{0rq.Ub0&C1&lPu\vP-VRqsi?:s)0"fG@]uiX-&AE]2FP
-?Rraq^MM[&:8V;GkuZM2T6_m2`h:saaVQoDo:"pKGi.CAaS@%?/NndT{V#iVo%Dkr]0)Wv
-ire/Dx5*u|:Ir%2rC"T""Uc{'ll150'@&!^Z#h_f?AKq9iIpXSDaU7GF6,P`MVtcJ>6gVI
-N6X,H5[7C:LbC/I\O>)zl+bYu;\P\]?XZmYfgE<rjEgTS0g&ZvhQ<rCVYTp=ntucTsmsEf
-f$1m%,Qa/Zb,JA.`,"F~'RI7^c!MSTsa:piD48nmL8++$ws2t]MO6\&7:#pK@8fU#/f?s9
-(FAD\1(6m*oE:",DNfL9dCmgLdKth}sYZg3I@f@R]BJKD)orLdKth}sYZgf\tL:%,DNfL9
-dCMG,"Rsg*,"Rs/bF0+DuZ/ps*)c/z?/[8;2&R:s"nRz(P<we2,iVVTSc`:NqOQ'\v8U>6
-@[2lqQnU hEh!k)O9\Dl@0I5T%c`8!:<kfSJtRnK)7c`50q.Lw852HtmSFc`)2($?r*)EB
-TwbBl+50bFl+T=!i&\Ael&50@-fB-Y+UZWfTp,k.hxSH^E<(V&5Mj&W>5MtpFodVLrNj9;
-d{50dDN%\$6lU8Z9SKBBDk@0I5:/ t_z,)cK6j/iF0NVb!UlJ0i#rc"djUYQS<=aDuk;D 
-iLL]OzirL]:EXomE&vMl?6D?Z(5lr`?v[clu0WUyVao[]'s9 2ufGt< szj"CkH"koDpn^
- ;Un, $%Y1(*$2V<a70_7TGtX<#&Dk@0I5HE_3G4EIGF[3rt?2Q^5Li:Tea70_rofa2O#=
-t~`#6st~U8ND@J5|X `#gDb`e&Vffjo*e'50$b?PHydV!gNOb!JANVb!Ul-3D[cM-bG=_X
-!>J M~D(1Xt0Ng#4Gs"<ba=Q72[@j%Kr9iW>VsMOsgIpCbXR)z/z[KOiT)8h'7"=RQOdiO
-L]OzirL]$oq:HW]6mdIAlmJtM*T6JR6X^^d(ZrD./}%FkH8$GMPBOfX,8%eG"<`3PQ8w&-
-KC.Ak{@XCetwg)gR8$3aEw"<bah\8'j!8 s1MV.M5A]/jsbA8$iW`@rJpjH0nI-Q%Cs2-n
-%CiX.2U%++$ws2t]MO6\&7ENO$EKsrb(bPd T9!CuZk,+IuZk,T#?2.$=A=qB<s1=^7UQ5
-)~q+B~bg04[v ~YZ=x#8!T[fp}?:W$GxkoASH"ko6ht~?bo}3H?u^>@:VE-%Ug%]/_1"Yo
-T.9T6v2qQ,T}lh+xCXQ,\La7EYllfu!RkQ#W2~S9iY`@rJ1sQ,P#TXa70_rofa2OQ+JAl&
-#Z:u]+jsbA8$iW`@rJpjH0nI-Q%Cs2-n%CiX.2U%++$ws2t]MO6\&7ENO$EKsrb(bPd T9
-!CuZk,+IuZk,T#?2.$=A=qB<s1=^7UQ5)~q+B~bg04[v ~YZ=xZGj<hTJ54Gl$Ys<+-V?P
-J{VGX0[&Oj<p$$);?! q5ct6CZK~f&DoaI#4!TfQ,_>?UpA81Yq}[Sen!S.b:Pt_adJpiC
-?/=+Ea(#4*DJXV$WmFbN]\@o>%TosaaV,*T?$w<{JbYkO|S<=aDu@0ibSO8h'7"=eD2G,s
-&2bNa7B1W$SP8h'7"=eD2GKEv)Tc5b S3rb h`Tc5b S3rANjw`&[;>XJuKx !
+(* ::Package:: *)
+
+(************************************************************************)
+(* This file was generated automatically by the Mathematica front end.  *)
+(* It contains Initialization cells from a Notebook file, which         *)
+(* typically will have the same name as this file except ending in      *)
+(* ".nb" instead of ".m".                                               *)
+(*                                                                      *)
+(* This file is intended to be loaded into the Mathematica kernel using *)
+(* the package loading commands Get or Needs.  Doing so is equivalent   *)
+(* to using the Evaluate Initialization Cells menu command in the front *)
+(* end.                                                                 *)
+(*                                                                      *)
+(* DO NOT EDIT THIS FILE.  This entire file is regenerated              *)
+(* automatically each time the parent Notebook file is saved in the     *)
+(* Mathematica front end.  Any changes you make to this file will be    *)
+(* overwritten.                                                         *)
+(************************************************************************)
+
+
+
+(* ::Input::Initialization:: *)
+contShdLst[col_,sigmas_,oplst_:{0.8,0.1}]:={Sequence@@(Blend[{Directive[Darker@col,Opacity[First@oplst]],Directive[White,Opacity[Last@oplst]]},1/#]&/@Range[Length@sigmas+1])}
+Options[custompost2D]=Join[Options[ContourPlot],Options[SmoothKernelDistribution]]~Join~{color->Blue,range->All,xyLabel->"Default",points->200,siglst->{1,2,3},contourStyle->None,contourShading->"Default"};
+
+
+(* ::Input::Initialization:: *)
+custompost2D[mcres_,parinds_List,opts:OptionsPattern[]]:=
+Module[{dat=(#[[parinds]]&/@mcres["ParameterRun"])[[mcres["BurnEnd"];;]],dist,rng,conlst,lab,cpl,shd},
+dist=SmoothKernelDistribution[dat,Evaluate[FilterRules[{opts},Options[SmoothKernelDistribution]]]];
+rng=If[OptionValue[range]===All,{MinMax[First/@#],MinMax[Last/@#]}&@dat,OptionValue[range]];
+conlst=conpdf[dist,OptionValue[points],OptionValue[siglst]][rng];
+If[OptionValue[xyLabel]=!="Default",
+If[!(ListQ[OptionValue[xyLabel]]&&Length[OptionValue[xyLabel]]===Length[parinds]),
+Message[Posterior2D::nomatch];Abort[]
+]
+];
+shd=If[OptionValue[contourShading]==="Default",Evaluate[contShdLst[OptionValue[color],OptionValue[siglst]]],OptionValue[contourShading]];
+lab=If[OptionValue[xyLabel]==="Default",ToString/@(mcres["Parameters"][[parinds]]),OptionValue[xyLabel]];
+cpl=
+ContourPlot[PDF[dist,{x,y}],Evaluate[{x,Sequence@@rng[[1]]}],Evaluate[{y,Sequence@@rng[[2]]}],Contours->conlst,ContourStyle->OptionValue[contourStyle],
+ContourShading->shd,PlotRange->All,Evaluate[FilterRules[{opts},Options[ContourPlot]]]];
+Show[cpl,FrameStyle->Directive[Black],FrameTicksStyle->Directive[Black,Italic,FontFamily->"Times",FontSize->20],
+FrameLabel->Evaluate[Style[#,Black,Italic,FontFamily->"Times",FontSize->26]&/@lab],GridLinesStyle->Directive[Gray,Dotted],ImageSize->Large,PlotRange->rng]
+]
+ClearAll[datawc]
+Options[datawc]={"CompilationTarget"->"WVM"};
+datawc[mcrun_,file_,extraconstraints_,OptionsPattern[]]:=
+Module[{modelparam,dataparam,exp},
+modelparam=mcrun["Parameters"];
+dataparam=(*#[[Range[Length@modelparam]]]&/@*)mcrun["ParameterRun"][[mcrun["BurnEnd"];;]];
+exp=Block[{funcc=((Last[#]*\[CapitalLambda]^2)&/@file[[2]])/.extraconstraints/.rplst},
+Compile[Evaluate[{#,_Real}&/@modelparam],funcc,
+RuntimeAttributes->{Listable},CompilationOptions->{"InlineExternalDefinitions"->True},RuntimeOptions->{"EvaluateSymbolically"->False,"WarningMessages"->False},
+Parallelization->True,CompilationTarget->OptionValue["CompilationTarget"]]
+];
+exp@@@dataparam
+]
+
+
+(* ::Input::Initialization:: *)
+ClearAll[wcposterior2D]
+Options[wcposterior2D]=Join[Options[ContourPlot],Options[SmoothKernelDistribution]]~Join~{extraconstraints-> {},color->Blue,range->All,xyLabel->"Default",points->300,siglst->{1,2,3},contourStyle->None,contourShading->"Default" };
+wcposterior2D[datawc_,param_List,modelfile_,opts:OptionsPattern[]]:=
+Module[{parinds,dat,exp,datwc,dist,rng,conlst,lab,shd,cpl},
+dat=datawc[[All,param]];
+dist=SmoothKernelDistribution[dat,Evaluate[FilterRules[{opts},Options[SmoothKernelDistribution]]]];
+rng=If[OptionValue[range]===All,{MinMax[First/@#],MinMax[Last/@#]}&@dat,OptionValue[range]];
+conlst=conpdf[dist,OptionValue[points],OptionValue[siglst]][rng];
+If[OptionValue[xyLabel]=!="Default",
+If[!(ListQ[OptionValue[xyLabel]]&&Length[OptionValue[xyLabel]]===Length[param]),
+Message[Posterior2D::nomatch];Abort[]
+]
+];
+lab=If[OptionValue[xyLabel]==="Default",First/@modelfile[[2]][[param]],OptionValue[xyLabel]];
+shd=If[OptionValue[contourShading]==="Default",Evaluate[contShdLst[OptionValue[color],OptionValue[siglst]]],OptionValue[contourShading]];
+cpl=
+ContourPlot[PDF[dist,{x,y}],Evaluate[{x,Sequence@@rng[[1]]}],Evaluate[{y,Sequence@@rng[[2]]}],Contours->conlst,ContourStyle->OptionValue[contourStyle],
+ContourShading->shd,PlotRange->All,Evaluate[FilterRules[{opts},Options[ContourPlot]]]];
+Show[cpl,FrameStyle->Directive[Black],FrameTicksStyle->Directive[Black,Italic,FontFamily->"Times",FontSize->15],
+FrameLabel->Evaluate[Style[#,Black,Italic,FontFamily->"Times",FontSize->26]&/@lab],GridLinesStyle->Directive[Gray,Dotted],ImageSize->Large,PlotRange-> rng]
+]
+
+
+(* ::Input::Initialization:: *)
+wcPlotFromModel[mcres_,mod_,extraconstraints_,wcs_,col_,sl_,rng_]:=Block[{params=Position[mod[[2]]/.replstwc,ToString[#]][[1,1]]&/@wcs},
+wcposterior2D[datawc[mcres,mod, extraconstraints],params,mod,color->col,siglst->sl,xyLabel->wcs/.stringlstwc,range->rng,points->300,contourStyle->None,contourShading->"Default",PerformanceGoal->"Quality",GridLines->Automatic,PlotPoints->70,AspectRatio->1]
+]
+
+
+(* ::Input::Initialization:: *)
+wcPlotFromModelCL[mcres_,mod_,extraconstraints_,wcs_,col_,sl_,rng_]:=Block[{params=Position[mod[[2]]/.replstwc,ToString[#]][[1,1]]&/@wcs},
+wcposterior2D[datawc[mcres,mod, extraconstraints],params,mod,color->col,siglst->sl,xyLabel->wcs/.stringlstwc,range->rng,points->300,contourStyle->{Directive[col,Dashed],Directive[col]},contourShading->None,PerformanceGoal->"Quality",GridLines->Automatic,PlotPoints->70,AspectRatio->1]
+]
+
+
+(* ::Input::Initialization:: *)
+legboxCL[col_]:=Show[RegionPlot[0<=x<=1&&0<=y<=1,{x,0,1},{y,0,1},Frame->False,BoundaryStyle->Directive[col,Thick,Dashed],PlotStyle->None],RegionPlot[0.25<=x<=0.75&&0.25<=y<=0.75,{x,0,1},{y,0,1},Frame->False,BoundaryStyle->Directive[col,Thick],PlotStyle->None]]
+
+
+(* ::Input::Initialization:: *)
+legboxFL[col_]:=Show[RegionPlot[0<=x<=1&&0<=y<=1,{x,0,1},{y,0,1},Frame->False,BoundaryStyle->None,PlotStyle->Directive[col,Opacity[0.3]]],RegionPlot[0.25<=x<=0.75&&0.25<=y<=0.75,{x,0,1},{y,0,1},Frame->False,BoundaryStyle->None,PlotStyle->Directive[col,Opacity[0.3]]]]
+ClearAll[customLegCL,customLegFL,customLegCL2,customLegFL2];
+customLegCL[lst_,colst_,lab_,size_:30,opts:OptionsPattern[]]:=
+With[{cls = Take[colst, Length@lst]},
+SwatchLegend[cls,lst,LegendMarkers->legboxCL/@cls,LegendMarkerSize->size,LabelStyle->Directive[Black,Italic,FontSize->size-10,FontFamily->"Times New Roman"],LegendLabel->Style[lab,size-10+2],opts]
+  ]
+customLegFL[lst_,colst_,lab_,size_:30,opts:OptionsPattern[]]:=
+With[{cls = Take[colst, Length@lst]},
+SwatchLegend[cls,lst,LegendMarkers->legboxFL/@cls,LegendMarkerSize->size,LabelStyle->Directive[Black,Italic,FontSize->size-10,FontFamily->"Times New Roman"],LegendLabel->Style[lab,size-10+2],opts]
+  ]
+customLegCL2[lst_,colst_,size_:30,opts:OptionsPattern[]]:=
+With[{cls = Take[colst, Length@lst]},
+SwatchLegend[cls,lst,LegendMarkers->legboxCL/@cls,LegendMarkerSize->size,LabelStyle->Directive[Black,Italic,FontSize->size-10,FontFamily->"Times New Roman"],opts]
+  ]
+customLegFL2[lst_,colst_,size_:30,opts:OptionsPattern[]]:=
+With[{cls = Take[colst, Length@lst]},
+SwatchLegend[cls,lst,LegendMarkers->legboxFL/@cls,LegendMarkerSize->size,LabelStyle->Directive[Black,Italic,FontSize->size-10,FontFamily->"Times New Roman"],opts]
+  ]
+
+
+(* ::Input::Initialization:: *)
+SetAttributes[{contShdLst,custompost2D,datawc,wcposterior2D,wcPlotFromModel,wcPlotFromModelCL,legboxCL,legboxFL,customLegCL,customLegFL,customLegCL2,customLegFL2},{Protected,ReadProtected,Locked}]
+
+
+
